31881c218a268e1708771b02055acde824b8a703
darya-zernina <123591698+darya-zernina@users.noreply.github.com>
2023-02-11 22:48:23 +0500

    Commit Task 6

--- Lesson_10.02.23_t6/Program.cs
+++ Lesson_10.02.23_t6/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число: ");
+int Number = int.Parse(Console.ReadLine());
+int x = Number % 2;
+if (x == 0);
+{
+    Console.WriteLine("да");
+}
+else  
+{
+    Console.WriteLine("Нет");
+}
