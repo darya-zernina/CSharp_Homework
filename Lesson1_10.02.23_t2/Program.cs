e72480f5011b593ec6f7ac554bb91c97486b1598
darya-zernina <123591698+darya-zernina@users.noreply.github.com>
2023-02-11 20:14:12 +0500

    Code

--- Lesson1_10.02.23_t2/Program.cs
+++ Lesson1_10.02.23_t2/Program.cs
@@ -3,5 +3,11 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-
+Console.Write("Введите число 1:  ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2:  ");
+int y = int.Parse(Console.ReadLine());
 Console.WriteLine("Hello, World!");
+if (x > y) 
+System.Console.WriteLine(x);
+else Console.WriteLine(y);
