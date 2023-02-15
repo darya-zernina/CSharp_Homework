d39162f32aad307d0d452af71eee06bd44cf198c
darya-zernina <123591698+darya-zernina@users.noreply.github.com>
2023-02-16 01:12:54 +0500

    Task10

--- Lesson2_14.02.2023_task_10/Program.cs
+++ Lesson2_14.02.2023_task_10/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и 
+//на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int a1 = num / 10;
+int a2 = a1 % 10;
+System.Console.WriteLine(a2);
+
