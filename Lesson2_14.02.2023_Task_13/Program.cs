7a043308fb767b32692ef6365089e6de6345921d
darya-zernina <123591698+darya-zernina@users.noreply.github.com>
2023-02-16 01:29:36 +0500

    Task_13

--- Lesson2_14.02.2023_Task_13/Program.cs
+++ Lesson2_14.02.2023_Task_13/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или
+// сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num>99 && num<1000)
+{
+  int a1 = num % 10; 
+  System.Console.WriteLine(a1);
+}
+else System.Console.WriteLine("Нет третьей цифры");
+
