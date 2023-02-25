f5fe4db3cdd13b6430b45bf4777bf72fd5144a35
darya-zernina <123591698+darya-zernina@users.noreply.github.com>
2023-02-25 13:09:40 +0500

    task34

--- Lesson5_24.02.2023_task34/Program.cs
+++ Lesson5_24.02.2023_task34/Program.cs
@@ -16,16 +16,19 @@ void PrintArray(int[] numbers)
     Console.Write("]");
     Console.WriteLine();
 }
+void FindSum(int[] inArray)
+{
+ int index = 0;
+for (int i = 0; i < inArray.Length; i++)
+if (inArray[i] % 2 == 0)
+index++;   
+}
+
 Console.WriteLine("Введите размер массива");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers);
 Console.WriteLine("Вот наш массив: ");
 PrintArray(numbers);
-int count = 0;
+Console.WriteLine($"всего {numbers.Length} чисел");
 
-for (int z = 0; z < numbers.Length; z++)
-if (numbers[z] % 2 == 0)
-count++;
-
-Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
