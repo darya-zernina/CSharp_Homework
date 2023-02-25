df97bfeed57689870c32307c0d8b186d6aa69092
darya-zernina <123591698+darya-zernina@users.noreply.github.com>
2023-02-25 11:55:30 +0500

    Task29

---
+++ Lesson4_22.02.2023_t_29/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] GetArray(int size, int minValue, int maxValue)
+ {   
+      int[] res = new int[size];//создание массива
+           for (int i = 0; i < size; i++)  
+              {         res[i] = new Random().Next(minValue, maxValue + 1);
+                   } 
+                  return res; 
+                  }  
+void PrintArray(int[] inArray)
+ {     
+    for (int i = 0; i < inArray.Length; i++)
+     {         Console.Write($"{inArray[i]} "); 
+         } 
+         } 
+         
+int[] array = GetArray(8, -9, 9);
+ PrintArray(array);
+ 
+  
