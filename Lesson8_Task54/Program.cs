313155ad88ee754221e8991bff6267a752878541
darya-zernina <123591698+darya-zernina@users.noreply.github.com>
2023-03-09 22:28:50 +0500

    Task54

---
+++ Lesson8_Task54/Program.cs
@@ -0,0 +1,54 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array [i, j] = Convert.ToInt32(new Random().Next(0,21));
+    }
+  }
+}
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+void OrderArrayLines(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        {
+          int temp = array[i, k + 1];
+          array[i, k + 1] = array[i, k];
+          array[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+CreateArray(array);
+PrintArray(array);
+Console.WriteLine(" ");
+OrderArrayLines(array);
+PrintArray(array);
