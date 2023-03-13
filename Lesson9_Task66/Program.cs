710bb8b9ecd788070ca244f010c3d202151708d6
darya-zernina <123591698+darya-zernina@users.noreply.github.com>
2023-03-14 00:23:45 +0500

    task66

---
+++ Lesson9_Task66/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int CountSumNums(int M, int N)
+{
+    if (M == N)
+        return N;
+    return N + CountSumNums(M, N - 1);
+}
+
+Console.WriteLine("Введите M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма элементов от {M} до {N} = {CountSumNums(M, N)}");
