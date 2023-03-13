4319263fba313bd0fd87eb9e2ab76e7b2f444c7c
darya-zernina <123591698+darya-zernina@users.noreply.github.com>
2023-03-14 00:13:59 +0500

    Task64

--- Lesson9_T64/Program.cs
+++ Lesson9_T64/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int start = 0;
+Consequense(N, start);
+
+int Consequense(int number, int start)
+{    
+    if (number == start)
+        return number;
+    else
+        Console.Write($"{Consequense(number, start + 1)}, ");
+    return start;
+} 
